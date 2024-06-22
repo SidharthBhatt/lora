2e2dc913a2162dfc0761b4bf1e1dd71eed8062aa
SidharthBhatt <sidharthbhatt201907@gmail.com>
2024-06-22 14:45:48 -0700

    wait this actually sends packets

--- start.py
+++ start.py
@@ -1,39 +1,71 @@
 import subprocess
 from subprocess import call
 import struct
-
-def capture_image(output_path='webcam_image.jpg'):
+import sys
+from time import sleep
+from SX127x.LoRa import *
+from SX127x.board_config import BOARD
+# import the python libraries
+output_path = 'webcam_image.jpg'
+subprocess.run([
+    'ffmpeg', '-f', 'video4linux2', '-i', '/dev/video0',
+    '-vframes', '1', output_path
+], check=True)
+print(f"Image saved as {output_path}")
     
-    # Run the FFmpeg command to capture a single frame from the webcam
-    subprocess.run([
-        'ffmpeg', '-f', 'video4linux2', '-i', '/dev/video0',
-        '-vframes', '1', output_path
-    ], check=True)
-    print(f"Image saved as {output_path}")
+#read image with a specified chunk size
+CHUNK_SIZE = 200
+f = open("webcam_image.jpg", 'rb')
+chunk = f.read(CHUNK_SIZE)
     
-    CHUNK_SIZE = 200
+BOARD.setup()
+# is used to set the board and LoRa parameters
+class LoRaBeacon(LoRa):
 
-    f = open("webcam_image.jpg", 'rb')
-    chunk = f.read(CHUNK_SIZE)
-    while chunk: #loop until the chunk is empty (the file is exhausted)
+    def __init__(self, verbose=False):
+        super(LoRaBeacon, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        # sleep to save power
+        self.set_dio_mapping([1,0,0,0,0,0])
+        #go to this web to read the doc: https://cdn-shop.adafruit.com/product-files/3179/sx1276_77_78_79.pdf
+    def start(self):
+        global args
+        self.write_payload([])
+        self.set_mode(MODE.TX)
+        while True:
+              sleep(1)
+    def on_tx_done(self):
+        self.set_mode(MODE.STDBY)
+        self.clear_irq_flags(TxDone=1)
+        sys.stdout.flush()
         
         chunk = bytearray(f.read(CHUNK_SIZE)) #read the next chunk
         numbers = list(chunk)
         print(len(numbers))
-        print("space")
-        #for x in chunk:
-            #print(x)
-    f.close()#open image as byte array
+        self.write_payload(numbers)
+        self.set_mode(MODE.TX)
+            
+
+
+lora = LoRaBeacon(verbose=False)
+
+lora.set_pa_config(pa_select=1)
+
+assert(lora.get_agc_auto_on() == 1)
+
+try: sleep(0.001)
+except: pass
+
+try:
+    # Run the FFmpeg command to capture a single frame from the webcam
     
-    """  
-    with open("webcam_image.jpg", "rb") as image:
-        f = image.read()
-        b = bytearray(f)
-        
-        for i in range(0, len(b), 250):
-            #print(b[i:i + 250].hex())
-        print(b[0])
-    """ 
     
-if __name__ == "__main__":
-    capture_image('webcam_image.jpg')
+    lora.start()
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    sys.stderr.write("KeyboardInterrupt\n")
+ #print the transmitted values on the console and terminate the program using a keyboard interrupt   
+finally:
+    sys.stdout.flush()
+    lora.set_mode(MODE.SLEEP)
+    BOARD.teardown()
