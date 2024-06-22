6672b3ec5d43d6af90b4ca785dc27460b60d1bbc
SidharthBhatt <sidharthbhatt201907@gmail.com>
2024-06-22 12:48:18 -0700

    still not working

--- start.py
+++ start.py
@@ -10,14 +10,27 @@ def capture_image(output_path='webcam_image.jpg'):
     ], check=True)
     print(f"Image saved as {output_path}")
     
-    #open image as byte array
+    CHUNK_SIZE = 430
+
+    f = open("webcam_image.jpg", 'rb')
+    chunk = f.read(CHUNK_SIZE)
+    while chunk: #loop until the chunk is empty (the file is exhausted)
+        
+        chunk = bytearray(f.read(CHUNK_SIZE)) #read the next chunk
+        print(chunk)
+        #for x in chunk:
+            #print(x)
+    f.close()#open image as byte array
+    
+    """  
     with open("webcam_image.jpg", "rb") as image:
         f = image.read()
         b = bytearray(f)
         
         for i in range(0, len(b), 250):
-            print(b[i:i + 250])
+            #print(b[i:i + 250].hex())
         print(b[0])
+    """ 
     
 if __name__ == "__main__":
     capture_image('webcam_image.jpg')
